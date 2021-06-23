5bb6b7d25112e20cb218cbeeda60b5c60ecaa154
liubq7 <liubq97@gmail.com>
2021-06-23 11:07:19 +0800

    timeline+btn animation

--- src/components/TXViz.js
+++ src/components/TXViz.js
@@ -1,11 +1,26 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useEffect } from "react";
 import TXNodes from "./TXNodes";
-import "../css/TXViz.css"
+import "../css/TXViz.css";
 
 const TXViz = () => {
   const startTime = 1622463474248;
   const endTime = 1622463476577;
   const [rangeval, setRangeval] = useState(startTime);
+  const [play, setPlay] = useState(false);
+  const [btnText, setBtnText] = useState("play");
+
+  console.log(rangeval);
+
+  useEffect(() => {
+    if (rangeval == endTime) {
+      setBtnText("play");
+      setPlay(false);
+    }
+    if (play) {
+      const timer = setInterval(() => setRangeval(rangeval + 1), 1);
+      return () => clearInterval(timer);
+    }
+  });
 
   return (
     <Fragment>
@@ -22,7 +37,22 @@ const TXViz = () => {
           value={rangeval}
           onChange={(event) => setRangeval(Number(event.target.value))}
         />
-        <button id="play-button" onClick={() => setRangeval(rangeval + 1)}>play</button>
+        <button
+          id="play-button"
+          onClick={() => {
+            if (rangeval == endTime) {
+              setRangeval(startTime);
+            }
+            if (play) {
+              setBtnText("play");
+            } else {
+              setBtnText("pause");
+            }
+            setPlay(!play);
+          }}
+        >
+          {btnText}
+        </button>
       </div>
     </Fragment>
   );
