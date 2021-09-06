15b464bdd61617b7f71631d4f899eeeafbd1ff53
liubq7 <liubq97@gmail.com>
2021-09-06 14:08:57 +0800

    change baseURL

--- client/src/apis/DataFinder.js
+++ client/src/apis/DataFinder.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NODE_ENV === "production" ? "/" : "http://localhost:3006";
+const baseURL = process.env.NODE_ENV === "production" ? "/api" : "http://localhost:3006";
 
 export default axios.create({
   baseURL,
